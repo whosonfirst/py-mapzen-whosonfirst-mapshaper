9571edb1dfc0ed9aeda4b94551ad4213e0a61b73
thisisaaronland <aaron@mapzen.com>
2016-01-30 01:33:00 +0000

    add utils class; bump to 0.05

--- setup.py
+++ setup.py
@@ -3,13 +3,23 @@
 # Remove .egg-info directory if it exists, to avoid dependency problems with
 # partially-installed packages (20160119/dphiffer)
 
-import os, sys
-from shutil import rmtree
+import os
+import sys
+import shutil
 
-cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
-egg_info = cwd + "/mapzen.whosonfirst.mapshaper.egg-info"
-if os.path.exists(egg_info):
-    rmtree(egg_info)
+setup = os.path.abspath(sys.argv[0])
+parent = os.path.dirname(setup)
+pkg = os.path.basename(parent)
+
+if pkg.startswith("py-mapzen"):
+    pkg = pkg.replace("py-", "")
+    pkg = pkg.replace("-", ".")
+
+    egg_info = "%s.egg-info" % pkg
+    egg_info = os.path.join(parent, egg_info)
+
+    if os.path.exists(egg_info):
+        shutil.rmtree(egg_info)
 
 from setuptools import setup, find_packages
 
